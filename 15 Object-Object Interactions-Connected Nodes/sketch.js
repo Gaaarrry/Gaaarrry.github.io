a5d05a0886305348d371cead54aaf7b730ce6a49
Gaaarrry <garyshunshun@gmail.com>
2025-03-18 11:25:43 -0600

    s

---
+++ 15 Object-Object Interactions-Connected Nodes/sketch.js
@@ -0,0 +1,13 @@
+// Connected Nodes 
+// Mr. Scott
+// March 18, 2025
+// OOP Reveiw + Object-Object Interactions
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
--- Terrain Starter/sketch.js
+++ Terrain Starter/sketch.js
@@ -1,10 +1,9 @@
 // Project Title
 // Gary Wang
 // March, 4th, 2025
-//
-//Starter Code for our
+
 //Terrain Generation Project
-let rectWidth = 0;
+let rectWidth = 1;// set the width of the noise----1
 
 
 
@@ -15,13 +14,14 @@ function setup() {
 function draw() {
   
   background(220);
-  if (keyIsDown(LEFT_ARROW)) {
+  //use Arrow key to interact with the terrain
+  if (keyIsDown(LEFT_ARROW)) {//left arrow decrease...
     rectWidth -=0.25;
-    if(rectWidth < 0){
-      rectWidth = 0;
+    if(rectWidth < 1){
+      rectWidth = 1;
     }
   }
-  if (keyIsDown(RIGHT_ARROW)) {
+  if (keyIsDown(RIGHT_ARROW)) {//right arrow increase...
     rectWidth += 0.25;
     if(rectWidth > 40){
       rectWidth = 40;
@@ -34,23 +34,28 @@ function draw() {
 }
 
 function generateTerrain(){
-  let peak = frameCount*0.03;
-  let peaky = height;
-  let peakx;
+  let peak = frameCount*0.03;//set the speed of the noise
+  let peakY = height;
+  let peakX;
+  let allHeight = 0;
+  let allRect = 0;
   for (let x = 1; x < width; x++) {
-    x += rectWidth;
+    x += rectWidth;//add the width of the noise
     let y = noise(peak) * height; //noise(peak-1 height increase through noise value decrease
    
     peak += 0.01;
     fill(0);
-    rect(x, y, rectWidth, height - y);
-    if(y < peaky){//find the highest point
-      peaky = y;
-      peakx = x;
+    rect(x, y, rectWidth, height - y);//use this to draw the terrain one by one
+    allHeight += y; //find all height of the terrain
+    allRect +=1;
+    if(y < peakY){//find the peak...
+      peakY = y;
+      peakX = x;
     }
    
   }
-  drawflag(peakx,peaky);
+  drawflag(peakX,peakY);//use peakX and peakY to put the flag on the top of the highest peak
+  averageHeight(allHeight/allRect);//the resault will be the average height
 }
 function drawflag(x,y){
   fill(255);
@@ -62,3 +67,8 @@ function drawflag(x,y){
 }
 
 
+function averageHeight(yValue){
+  fill(255,0,0);
+  rect(0,yValue,width, 5);//starts at 0, yValue(the average height)
+                          //screen's width, and height   5
+}
