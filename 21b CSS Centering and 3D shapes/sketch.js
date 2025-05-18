9f75d24b472eccb0f654f28d7bfe5607b6006d15
Gaaarrry <garyshunshun@gmail.com>
2025-05-18 17:37:44 -0600

    123

--- 21b CSS Centering and 3D shapes/sketch.js
+++ 21b CSS Centering and 3D shapes/sketch.js
@@ -5,32 +5,50 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let angle = 10;
+let angle = 60;
+
 function setup() {
-  createCanvas(500,500,WEBGL);
+  createCanvas(500, 500, WEBGL);
   angleMode(DEGREES);
+  
 }
 
 function draw() {
-  background(220); //WEGAL has 0,0 at the center
+  orbitControl();
+  background(220);  //WEBGL has 0,0 at the center
+  lights();
   rotateX(-20);
   rotateY(frameCount);
+  
   fill(100,240,100);
-  angle =map(mouseX,0,width,-120,120);
-  for(let i = 0; i< 360; i+=45){
+  angle = map(mouseX, 0, width, -120, 120);
+  for(let i = 0; i < 360; i+=20){
     push();
     rotateY(i);
-    drawBox(30);
+    drawBox(20);
     pop();
   }
   
 }
 
 function drawBox(size){
-  if(size>3){
+  if(size > 3){
+    let long = map(size, 20, 0, 0, 100);
+    push();
+    translate(0, -long / 2, 0);
+    box(size / 4, long, size / 4);
+    pop();
+
+    let longg = map(size, 20, 0, 0, 100);
+    push();
+    translate(0, longg / 2, 0);
+    box(-size / 4, -longg, size / 4);
+    pop();
+
     rotateZ(angle);
-    translate(size*1.5,0);
+    translate(size*2,0);
     box(size);
+
     drawBox(size*0.8);
   }
 }
