de8932e9a9dd40e28eca1515c3f07c26ff019255
Gaaarrry <garyshunshun@gmail.com>
2025-05-20 12:17:19 -0600

    12333

--- Fractal Project/sketch.js
+++ Fractal Project/sketch.js
@@ -11,22 +11,22 @@ function setup() {//in order to set the rectangle's mode and make it run
 function draw() {//to draw out the square that regulate by the functions
   background(0);
   randomSeed(1);
-  let topDepth = int(map(mouseX, 0, width, 1, 5));
-  squareFractal(width/2, height/2, height/2, 0, topDepth);
+  let side = int(map(mouseX, 0, width, 1, 5));
+  squareFractal(width/2, height/2, height/2, 0, side);
 }
-function squareFractal(x, y, sideL, depth, topDepth) {//to draw out the squares
+function squareFractal(x, y, sideL, depth, side) {//to draw out the squares
                      //x and y are the center of the square, sideL is the length of the square, depth is the depth, and topDepth is the maximum depth
   push();//push will store the current state
   translate(x, y);//translate the square to the center of the screen
   rotate(radians(frameCount)); //it will rotate
-  fill(random(255), random(255), random(255));//random color for the square
-  rect(0, 0, sideL);//draw the square
+  fill(random(255), random(255),random(255));//random color for the square
+  rect(0, 0,sideL);//draw the square
   pop();//pop will restore the state to the previous state
-  if (depth < topDepth) {//if the depth is less than the top depth, it will draw the square
+  if(depth < side){//if the depth is less than side, it will draw the square
     let addS = sideL / 2;
-    squareFractal(x - sideL/2, y -sideL/2,addS, depth+1, topDepth);
-    squareFractal(x + sideL/2, y - sideL/2, addS, depth+1, topDepth);
-    squareFractal(x -sideL/2, y + sideL/2, addS, depth+1, topDepth);
-    squareFractal(x+sideL/2, y +sideL/2, addS, depth+1, topDepth);
+    squareFractal(x - sideL/2, y - sideL/2,addS, depth+1, side);
+    squareFractal(x + sideL/2, y - sideL/2, addS, depth+1, side);
+    squareFractal(x - sideL/2, y + sideL/2, addS, depth+1, side);
+    squareFractal(x + sideL/2, y + sideL/2, addS, depth+1, side);
   }
 }
