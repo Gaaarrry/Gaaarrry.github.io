bd6c3bbd985fa4ff42600b3819107a160fb2ea1e
Gaaarrry <garyshunshun@gmail.com>
2025-06-11 12:16:12 -0600

    123213

---
+++ Mapppp/sketch.js
@@ -0,0 +1,26 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let textFile;
+
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  processText();
+}
+
+function draw() {
+  background(220);
+}
+
+function processText(){//split() spread syntax
+  print("")
+
+}
